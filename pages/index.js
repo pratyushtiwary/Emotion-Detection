c2a7eddc99f4a031518deb535a1a9ac088ed8a8a
= <cantaloupeinc.1@gmail.com>
2021-08-14 16:55:33 +0530

    Reduced file size for uploading from 5mb to 1mb

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home(props) {
           <h1 className={styles.title}>{props.appName}</h1>
           <h2 className={styles.pitch}>Recognise Your Emotions!</h2>
           <Uploader
-            maxSize = {5}
+            maxSize = {1}
             onUpload = {upload}
             accept = ".jpeg,.jpg,.png"
           />
@@ -82,7 +82,7 @@ export default function Home(props) {
         <h1 className={styles.title}>{props.appName}</h1>
         <h2 className={styles.pitch}>Recognise Your Emotions!</h2>
         <Uploader
-          maxSize = {5}
+          maxSize = {1}
           onUpload = {upload}
           accept = ".jpeg,.jpg,.png"
         />
